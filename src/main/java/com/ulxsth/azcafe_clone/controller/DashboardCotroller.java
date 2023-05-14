925f245dc74452e9057900ed085515d416df5517
ulxsth <ulxsth@gmail.com>
2023-05-14 13:15:50 +0900

    init

--- src/main/java/com/ulxsth/azcafe_clone/controller/DashboardCotroller.java
+++ src/main/java/com/ulxsth/azcafe_clone/controller/DashboardCotroller.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class UserController {
-    @RequestMapping("/login")
-    public String login() {
-        return "/user/login";
+public class DashboardCotroller {
+    @RequestMapping("/dashboard")
+    public String dashboard() {
+        return "/dashboard";
     }
 }
